9dbed08262541cd833f5788c3c599d36306ae497
BluieBlue <137467718+BluieBlue@users.noreply.github.com>
2023-06-29 22:05:30 -0400

    added color key
    utilizing airnow's color coding and signifance of aqi, the website depicts the aqi with its corresponding danger level

--- home.js
+++ home.js
@@ -1,54 +1,4 @@
-function calculateAQI() {
-    alert("AQI is some value. Working on it.");
-}
-
-function getGeolocation() {
-    if ("geolocation" in navigator) {
-        // check if geolocation is supported/enabled on current browser
-        navigator.geolocation.getCurrentPosition(
-         function success(position) {
-           // for when getting location is a success
-           alert('Successful. You are situated at ' + position.coords.latitude + ", " + position.coords.longitude);
-         },
-        function error(error_message) {
-          // for when getting location results in an error
-          alert('An error has occurred while retrieving location', error_message);
-          return [];
-        }  
-      );
-      } else {
-        // geolocation is not supported
-        // get your location some other way
-        console.log('geolocation is not enabled on this browser')
-        return [];
-      }
-}
-
 function getAQI() {
-    if ("geolocation" in navigator) {
-        // check if geolocation is supported/enabled on current browser
-        navigator.geolocation.getCurrentPosition(
-         function success(position) {
-           // for when getting location is a success
-           const lat = position.coords.latitude;
-           const lon = position.coords.longitude;
-           const url = `https://api.waqi.info/feed/geo:${lat};${lon}/?token=45e9661d7db44c176518a27b4e0a30bb1e3abbd1`;
-           let aqi = 0;
-           fetch(url).then(response => response.json()).then(data => alert(`The AQI is currently ${data.aqi}`));
-         },
-        function error(error_message) {
-          // for when getting location results in an error
-          alert('An error has occurred while retrieving location', error_message);
-        }  
-      );
-      } else {
-        // geolocation is not supported
-        // get your location some other way
-        console.log('geolocation is not enabled on this browser')
-      }
-}
-
-function getAQI2() {
     if ("geolocation" in navigator) {
         // check if geolocation is supported/enabled on current browser
         navigator.geolocation.getCurrentPosition(
@@ -56,7 +6,7 @@ function getAQI2() {
            // for when getting location is a success
            const latitude = position.coords.latitude;
            const longitude = position.coords.longitude;
-           const TOKEN = '45e9661d7db44c176518a27b4e0a30bb1e3abbd1'; // Replace with your actual WAQI API token
+           const TOKEN = '45e9661d7db44c176518a27b4e0a30bb1e3abbd1';
            const url = `https://api.waqi.info/feed/geo:${latitude};${longitude}/?token=${TOKEN}`;
            fetch(url)
               .then(response => response.json())
@@ -64,7 +14,8 @@ function getAQI2() {
                  if (data.status === 'ok') {
                     const aqi = data.data.aqi;
                     console.log(`The AQI at (${latitude}, ${longitude}) is ${aqi}`);
-                    document.getElementById("display").innerHTML = `The AQI is ${aqi}`;
+                    document.getElementById("display").innerHTML = `The AQI is ${aqi} \n`;
+                    aqiEvaluate(aqi);
                     // Do something with the AQI value
                  } else {
                     console.error('Unable to retrieve AQI data');
@@ -88,10 +39,24 @@ function getAQI2() {
       }
   }
   
-function getAQI3() {
-  const url = "https://api.techniknews.net/ipgeo/";
-  let aqi = 0;
-  fetch(url)
-     .then((response) => response.json())
-     .then((json) => console.log(json));
+function aqiEvaluate(aqi) {
+  if (aqi < 51) {
+    document.getElementById("display2").innerHTML = `The AQI is good. The air quality is satisfactory, and air pollution poses little or no risk.`;
+    document.querySelector(".output").style.backgroundColor = "green";
+  } else if (aqi < 101) {
+    document.getElementById("display2").innerHTML = `The AQI is moderate. Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution.`;
+    document.querySelector(".output").style.backgroundColor = "yellow";
+  } else if (aqi < 151) {
+    document.getElementById("display2").innerHTML = `The AQI is unhealthy for sensitive groups. Members of sensitive groups may experience health effects. The general public is less likely to be affected.`;
+    document.querySelector(".output").style.backgroundColor = "orange";
+  } else if (aqi < 201) {
+    document.getElementById("display2").innerHTML = `The AQI is unhealthy. Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.`;
+    document.querySelector(".output").style.backgroundColor = "red";
+  } else if (aqi < 301) {
+    document.getElementById("display2").innerHTML = `The AQI is very unhealthy. Health alert: The risk of health effects is increased for everyone.`;
+    document.querySelector(".output").style.backgroundColor = "purple";
+  } else {
+    document.getElementById("display2").innerHTML = `The AQI is hazardous. Health warning of emergency conditions: everyone is more likely to be affected.`;
+    document.querySelector(".output").style.backgroundColor = "maroon";
+  }
 }
